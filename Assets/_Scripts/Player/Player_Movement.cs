b74f7db7865e0e6082a561426cfc30d863ef28e1
percydavis03 <kdavi260@pratt.edu>
2024-09-23 21:32:37 -0400

    new nyx model

--- Assets/_Scripts/Player/Player_Movement.cs
+++ Assets/_Scripts/Player/Player_Movement.cs
@@ -12,16 +12,17 @@ public class Player_Movement : MonoBehaviour
     //FOR THE MOVEMENT:
     //new:
     public int speed;
+    public Transform playerTransform;
+    public float rotationSpeed;
 
-
-    //public Animator anim;
+    public Animator anim;
     //public Camera playerCamera;
     //public float walkSpeed = 6f;
     //public float runSpeed = 12f;
     public float jumpPower = 0f;
     public float gravity = 10f;
-    //public float lookSpeed = 2f;
-    //public float lookXLimit = 45f;
+    public float lookSpeed = 2f;
+    public float lookXLimit = 45f;
     public float defaultHeight;
     public float crouchHeight;
     public float crouchSpeed = 3f;
@@ -29,6 +30,8 @@ public class Player_Movement : MonoBehaviour
     private float rotationX = 0;
     private CharacterController characterController;
     private bool canMove = true;
+
+    public GameObject animationSource;
 
     private void Awake()
     {
@@ -45,9 +48,9 @@ public class Player_Movement : MonoBehaviour
         //Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
 
-        
+        anim = animationSource.GetComponent<Animator>();
 
-       
+
     }
 
     void Update()
@@ -63,15 +66,42 @@ public class Player_Movement : MonoBehaviour
         float movementDirectionY = moveDirection.y;
         moveDirection = (forward * curSpeedX) + (right * curSpeedY);
 
+        //for making the character face forwards
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        Vector3 rotationDirection = new Vector3(horizontalInput, 0, verticalInput);
+        rotationDirection.Normalize();
+
+        if (moveDirection.x != 0)
+        {
+            print("im runnin");
+            anim.SetBool("isRun", true);
+        }
+
+        if (moveDirection.z != 0)
+        {
+            print("im runnin");
+            anim.SetBool("isRun", true);
+        }
+
+        if(moveDirection.x == 0 && moveDirection.y == 0 && moveDirection.z == 0)
+        {
+            print("chill out time");
+            anim.SetBool("isRun", false);
+        }
+
+
         if (Input.GetKey(KeyCode.Space) && canMove && characterController.isGrounded)
         {
             if (thisGameSave.canJump == true)
             {
                 moveDirection.y = jumpPower;
+                anim.SetBool("isJump", true);
             }
             else if (thisGameSave.canJump == false)
             {
                 moveDirection.y = 0;
+                anim.SetBool("isJump", false);
                 print("loser cant jump :(");
             }
             
@@ -85,9 +115,21 @@ public class Player_Movement : MonoBehaviour
         if (!characterController.isGrounded)
         {
             moveDirection.y -= gravity * Time.deltaTime;
+        }
 
+        if (characterController.isGrounded == true)
+        {
+            anim.SetBool("isJump", false);
         }
 
         characterController.Move(moveDirection * Time.deltaTime);
+
+
+        if (rotationDirection != Vector3.zero)
+       {
+           Quaternion toRotation = Quaternion.LookRotation(rotationDirection, Vector3.up);
+
+            playerTransform.rotation = Quaternion.RotateTowards(playerTransform.rotation, toRotation, rotationSpeed * Time.deltaTime);
+       }
     }
 }
